d20edfd7843d8b9a14b1cae78c8d9639fd5ae62e
arcmags <c.magyar.ec@gmail.com>
2022-12-06 01:46:37 -0600

    qmk tweaks

--- dat/qmk/layouts/60_ansi/umap/keymap.c
+++ dat/qmk/layouts/60_ansi/umap/keymap.c
@@ -40,17 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     |-----------------------------------------------------------|
     |  ~  | ~ | ~ |Msu| ~ |Hom| ~ | ~ | ~ | ~ |PgU| ~ | ~ | Tg2 |
     |-----------------------------------------------------------|
-    |      |MWU|MsL|MsD|MsL| ~ |Lft|Dwn|Up |Rgt| ~ | ~ |        |
+    |      | ~ |MsL|MsD|MsL| ~ |Lft|Dwn|Up |Rgt|MWU| ~ |        |
     |-----------------------------------------------------------|
-    |        |MWD|Del| ~ | ~ |End|PgD|Ms1|Ms3|Ms2| ~ |          |
+    |        | ~ |Del| ~ | ~ |End|PgD|Ms1|Ms3|Ms2|MWD|          |
     |-----------------------------------------------------------|
     |     |    |    |          ~            |    | ~  |    |    |
     `-----------------------------------------------------------'*/
     [1] = LAYOUT_60_ansi(
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         KC_NO,   KC_NO,   KC_NO,   KC_MS_U, KC_NO,   KC_HOME, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_NO,   KC_NO,   TG(2),
-        _______, KC_WH_U, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,            _______,
-        _______,          KC_WH_D, KC_DEL,  KC_NO,   KC_NO,   KC_END,  KC_PGDN, KC_BTN1, KC_BTN3, KC_BTN2, KC_NO,            _______,
+        _______, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_WH_U, KC_NO,            _______,
+        _______,          KC_NO,   KC_DEL,  KC_NO,   KC_NO,   KC_END,  KC_PGDN, KC_BTN1, KC_BTN3, KC_BTN2, KC_WH_D,          _______,
         _______, _______, _______,                            KC_NO,                              _______, KC_NO,   _______, _______
     ),
 
